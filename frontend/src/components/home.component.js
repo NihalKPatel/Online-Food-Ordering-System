a31bc5c0e12b4403d4114a7df173e6b87932144d
NihalKPatel <70577135+NihalKPatel@users.noreply.github.com>
2022-09-14 02:02:35 +1200

    updated homepage

--- frontend/src/components/home.component.js
+++ frontend/src/components/home.component.js
@@ -2,6 +2,8 @@ import React, {Component} from "react";
 
 import UserService from "../services/user.service";
 import logo from "../images/logo_name_white.png";
+import {AiOutlineSearch} from "react-icons/ai";
+
 export default class Home extends Component {
     constructor(props) {
         super(props);
@@ -33,9 +35,16 @@ x
         return (
             <div className="home home-section">
                 <div>
-                    <header className="container" style={{paddingTop:"150px"}}>
-                        <img src={logo} style={{display:"block", marginLeft:"auto", marginRight: "auto",height:"218px", width:"612px"}}/>
-                        <p style={{textAlign:"center", color:"white", paddingTop:"50px", fontSize:"25px" }}>Find Local Restaurants</p>
+                    <header className="container" style={{paddingTop:"330px"}}>
+                        <img src={logo} style={{display:"block", marginLeft:"auto", marginRight: "auto", height:"106px", width:"309px"}}/>
+                        <p style={{textAlign:"center", color:"white", paddingTop:"60px", fontSize:"25px" }}>Find Local Restaurants</p>
+                        <div style={{paddingTop:"50px"}}>
+                        <form>
+                            <AiOutlineSearch style={{fontSize:"50px", marginTop:"15px", marginLeft:"10px", transform:"scale(-1, 1)"}}/>
+                            <input className="searchbar" type="search" placeholder="Search.."/>
+                            <button type="submit">Search</button>
+                        </form>
+                            </div>
                     </header>
                 </div>
             </div>
